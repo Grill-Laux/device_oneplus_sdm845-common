2db6738a200c1755fcb6da1f5104d3deaae6e8d2
uwu-gl <moegluwu0@gmail.com>
2024-06-28 17:45:58 +0800

    sdm845-common: Add sign sh

---
+++ sign.sh
@@ -0,0 +1,4 @@
+subject='/C=CN/ST=Shandong/L=Qingdao/O=gluwu/OU=gluwu/CN=gluwu/emailAddress=moegluwu0@gmail.com'
+for x in releasekey platform shared media networkstack verity otakey testkey sdk_sandbox bluetooth; do \
+    ./development/tools/make_key vendor/aosp/signing/keys/$x "$subject"; \
+done
