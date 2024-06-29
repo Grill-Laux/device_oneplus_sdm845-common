e3ce94feddfca3882bc449520a4961921de91fb4
uwu-gl <moegluwu0@gmail.com>
2024-06-29 08:56:36 +0800

    sdm845-common: fixup! Remove vendor.oneplus.hardware.display@1.0.vendor
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- common.mk
+++ common.mk
@@ -246,7 +246,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator@1.0-service \
     vendor.qti.hardware.display.config-V5-ndk \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapperextensions@1.0.vendor \
+    vendor.qti.hardware.display.mapperextensions@1.0.vendor
 
 # Doze
 PRODUCT_PACKAGES += \
