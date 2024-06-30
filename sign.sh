ecc6f339128d604788bf4add55268ee6ece11081
gluwu <137363994+uwu-gl@users.noreply.github.com>
2024-06-30 23:52:50 +0800

    sdm845-common: Sign NFC

--- sign.sh
+++ sign.sh
@@ -1,4 +1,4 @@
 subject='/C=CN/ST=Shandong/L=Qingdao/O=gluwu/OU=gluwu/CN=gluwu/emailAddress=moegluwu0@gmail.com'
-for x in releasekey platform shared media networkstack verity otakey testkey sdk_sandbox bluetooth; do \
+for x in releasekey platform shared media networkstack verity otakey testkey sdk_sandbox bluetooth nfc; do \
     ./development/tools/make_key vendor/aosp/signing/keys/$x "$subject"; \
 done
